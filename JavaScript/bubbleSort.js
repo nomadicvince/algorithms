2e203d28ca2d8cadd3f1249933526fe77c23c630
Vincent Moore <cosmiqwarrior@gmail.com>
2018-02-17 17:47:29 -0500

    Updated to ES6

--- JavaScript/bubbleSort.js
+++ JavaScript/bubbleSort.js
@@ -1,8 +1,8 @@
 function bubbleSort(array) {
-  for (var i = array.length; i > 0; i--) {
-    for (var j = 0; j < i; j++) {
+  for (let i = array.length; i > 0; i--) {
+    for (let j = 0; j < i; j++) {
       if (array[j] > array[j + 1]) {
-        var temp = array[j];
+        let temp = array[j];
         array[j] = array[j + 1];
         array[j + 1] = temp;
       }
