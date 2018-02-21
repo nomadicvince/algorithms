0f9eaad313b2436556c76b6f1a3708068027a01a
Vincent Moore <cosmiqwarrior@gmail.com>
2018-02-21 15:31:38 -0500

    Update sieveOfEratosthenes.js

--- JavaScript/sieveOfEratosthenes.js
+++ JavaScript/sieveOfEratosthenes.js
@@ -1,22 +1,22 @@
 /*Algorithm that returns all prime numbers up to the 'num' in an array*/
 
 function sieveofEratosthenes(n) {
-  var primes = [];
-  for (var i = 0; i <= n; i++) {
+  const primes = [];
+  for (let i = 0; i <= n; i++) {
     primes[i] = true;
   }
 
   primes[0] = false;
   primes[1] = false;
 
-  for (var i = 2; i <= Math.sqrt(n); i++) {
-    for (var j = 2; j * i <= n; j++) {
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    for (let j = 2; j * i <= n; j++) {
       primes[i * j] = false;
     }
   }
 
-  var result = [];
-  for (var i = 0; i < primes.length; i++) {
+  const result = [];
+  for (let i = 0; i < primes.length; i++) {
     if (primes[i]) {
       result.push(i);
     }
