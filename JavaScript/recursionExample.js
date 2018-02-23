81a14d8c48713804e5c6df16653b71381c2bd36c
Vincent Moore <cosmiqwarrior@gmail.com>
2018-02-23 12:18:12 -0500

    Update recursionExample.js
    Added a deeper explanation for recursion

--- JavaScript/recursionExample.js
+++ JavaScript/recursionExample.js
@@ -1,13 +1,22 @@
-// Recursion - when a function calls itself
+/*
+Recursion tips
+1. Figure out bare minimum pieces of info to represent your problem
+2. Give reasonable defaults to the bare minimum pieces of info
+3. Check the base case. Is there are work to do? If not, return
+4. Do some work, call your function again and make sure the arguments have changed in some fashion
+*/
 
-//recursion example
+function printNumber(n, dec = 1) {
+	//base case. Always provide a base case
+	if (n === 0) {
+		return;
+	}
 
-function factorial(num) {
-  if (num === 1) {
-    return num;
-  } else {
-    return num * factorial(num -1);
-  }
+	console.log(n);
+	/*
+  always change recursion in some manner to avoid infinite recursive loop
+  */
+	printNumber(n - dec);
 }
 
-factorial(5); //returns 120
+printNumber(12);
